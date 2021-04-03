a26eae2003e02c78884b00cf37dd6cdc15ae6cbe
Ravindu Ruwanpathirana <ravindur@sml.com>
2021-04-03 22:03:53 +0530

    Add UnitTest project for solution

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StuNote.Domain;
+using StuNote.Domain.Services;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+
+
+            //Act
+            
+
+            //Assert
+            //Assert.AreEqual();
+        }
+
+    }
+}

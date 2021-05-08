3df7c9949e9e00826fabe6e079554570e3733cc6
Ravindu Ruwanpathirana <ravind2k20@gmail.com>
2021-05-08 14:07:32 +0530

    Design changes

--- StuNote.Student/FormQuestionAnswer.Designer.cs
+++ StuNote.Student/FormQuestionAnswer.Designer.cs
@@ -30,86 +30,139 @@ namespace StuNote.Student
         private void InitializeComponent()
         {
             this.memoEditStuQuestion = new DevExpress.XtraEditors.MemoEdit();
-            this.lControlStuQuestion = new DevExpress.XtraEditors.LabelControl();
-            this.labelControlStuSelectAnswer = new DevExpress.XtraEditors.LabelControl();
             this.radioGroupStuAnswerSection = new DevExpress.XtraEditors.RadioGroup();
             this.simpBtnQuestAnsSubmit = new DevExpress.XtraEditors.SimpleButton();
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.radioGroupStuAnswerSectionitem = new DevExpress.XtraLayout.LayoutControlItem();
+            this.memoEditStuQuestionitem = new DevExpress.XtraLayout.LayoutControlItem();
+            this.simpBtnQuestAnsSubmititem = new DevExpress.XtraLayout.LayoutControlItem();
             ((System.ComponentModel.ISupportInitialize)(this.memoEditStuQuestion.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radioGroupStuAnswerSection.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FormQuestionAnswerlayoutControl1ConvertedLayout)).BeginInit();
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroupStuAnswerSectionitem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEditStuQuestionitem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.simpBtnQuestAnsSubmititem)).BeginInit();
             this.SuspendLayout();
             // 
             // memoEditStuQuestion
             // 
             this.memoEditStuQuestion.Enabled = false;
-            this.memoEditStuQuestion.Location = new System.Drawing.Point(234, 37);
+            this.memoEditStuQuestion.Location = new System.Drawing.Point(89, 12);
             this.memoEditStuQuestion.Name = "memoEditStuQuestion";
-            this.memoEditStuQuestion.Size = new System.Drawing.Size(314, 75);
+            this.memoEditStuQuestion.Size = new System.Drawing.Size(822, 213);
+            this.memoEditStuQuestion.StyleController = this.FormQuestionAnswerlayoutControl1ConvertedLayout;
             this.memoEditStuQuestion.TabIndex = 0;
-            // 
-            // lControlStuQuestion
-            // 
-            this.lControlStuQuestion.Location = new System.Drawing.Point(77, 67);
-            this.lControlStuQuestion.Name = "lControlStuQuestion";
-            this.lControlStuQuestion.Size = new System.Drawing.Size(63, 19);
-            this.lControlStuQuestion.TabIndex = 1;
-            this.lControlStuQuestion.Text = "Question";
-            // 
-            // labelControlStuSelectAnswer
-            // 
-            this.labelControlStuSelectAnswer.Location = new System.Drawing.Point(77, 177);
-            this.labelControlStuSelectAnswer.Name = "labelControlStuSelectAnswer";
-            this.labelControlStuSelectAnswer.Size = new System.Drawing.Size(65, 19);
-            this.labelControlStuSelectAnswer.TabIndex = 2;
-            this.labelControlStuSelectAnswer.Text = "Answers ";
             // 
             // radioGroupStuAnswerSection
             // 
-            this.radioGroupStuAnswerSection.Location = new System.Drawing.Point(234, 158);
+            this.radioGroupStuAnswerSection.Location = new System.Drawing.Point(89, 229);
             this.radioGroupStuAnswerSection.Name = "radioGroupStuAnswerSection";
             this.radioGroupStuAnswerSection.Properties.Items.AddRange(new DevExpress.XtraEditors.Controls.RadioGroupItem[] {
             new DevExpress.XtraEditors.Controls.RadioGroupItem(0, "Answer1"),
             new DevExpress.XtraEditors.Controls.RadioGroupItem(1, "Answer2"),
             new DevExpress.XtraEditors.Controls.RadioGroupItem(3, "Answer3"),
             new DevExpress.XtraEditors.Controls.RadioGroupItem(3, "Answer4")});
-            this.radioGroupStuAnswerSection.Size = new System.Drawing.Size(344, 181);
+            this.radioGroupStuAnswerSection.Size = new System.Drawing.Size(822, 180);
+            this.radioGroupStuAnswerSection.StyleController = this.FormQuestionAnswerlayoutControl1ConvertedLayout;
             this.radioGroupStuAnswerSection.TabIndex = 3;
             // 
             // simpBtnQuestAnsSubmit
             // 
-            this.simpBtnQuestAnsSubmit.Location = new System.Drawing.Point(234, 376);
+            this.simpBtnQuestAnsSubmit.Location = new System.Drawing.Point(12, 413);
             this.simpBtnQuestAnsSubmit.Name = "simpBtnQuestAnsSubmit";
-            this.simpBtnQuestAnsSubmit.Size = new System.Drawing.Size(349, 56);
+            this.simpBtnQuestAnsSubmit.Size = new System.Drawing.Size(899, 47);
+            this.simpBtnQuestAnsSubmit.StyleController = this.FormQuestionAnswerlayoutControl1ConvertedLayout;
             this.simpBtnQuestAnsSubmit.TabIndex = 4;
             this.simpBtnQuestAnsSubmit.Text = "Submit";
             this.simpBtnQuestAnsSubmit.Click += new System.EventHandler(this.simpBtnQuestAnsSubmit_Click);
+            // 
+            // FormQuestionAnswerlayoutControl1ConvertedLayout
+            // 
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout.Controls.Add(this.radioGroupStuAnswerSection);
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout.Controls.Add(this.memoEditStuQuestion);
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout.Controls.Add(this.simpBtnQuestAnsSubmit);
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout.Location = new System.Drawing.Point(0, 0);
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout.Name = "FormQuestionAnswerlayoutControl1ConvertedLayout";
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout.Root = this.layoutControlGroup1;
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout.Size = new System.Drawing.Size(923, 472);
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout.TabIndex = 5;
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.radioGroupStuAnswerSectionitem,
+            this.memoEditStuQuestionitem,
+            this.simpBtnQuestAnsSubmititem});
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(923, 472);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // radioGroupStuAnswerSectionitem
+            // 
+            this.radioGroupStuAnswerSectionitem.Control = this.radioGroupStuAnswerSection;
+            this.radioGroupStuAnswerSectionitem.Location = new System.Drawing.Point(0, 217);
+            this.radioGroupStuAnswerSectionitem.Name = "radioGroupStuAnswerSectionitem";
+            this.radioGroupStuAnswerSectionitem.Size = new System.Drawing.Size(903, 184);
+            this.radioGroupStuAnswerSectionitem.Text = "Answers ";
+            this.radioGroupStuAnswerSectionitem.TextLocation = DevExpress.Utils.Locations.Left;
+            this.radioGroupStuAnswerSectionitem.TextSize = new System.Drawing.Size(65, 19);
+            // 
+            // memoEditStuQuestionitem
+            // 
+            this.memoEditStuQuestionitem.Control = this.memoEditStuQuestion;
+            this.memoEditStuQuestionitem.Location = new System.Drawing.Point(0, 0);
+            this.memoEditStuQuestionitem.Name = "memoEditStuQuestionitem";
+            this.memoEditStuQuestionitem.Size = new System.Drawing.Size(903, 217);
+            this.memoEditStuQuestionitem.Text = "Question";
+            this.memoEditStuQuestionitem.TextLocation = DevExpress.Utils.Locations.Left;
+            this.memoEditStuQuestionitem.TextSize = new System.Drawing.Size(65, 19);
+            // 
+            // simpBtnQuestAnsSubmititem
+            // 
+            this.simpBtnQuestAnsSubmititem.Control = this.simpBtnQuestAnsSubmit;
+            this.simpBtnQuestAnsSubmititem.Location = new System.Drawing.Point(0, 401);
+            this.simpBtnQuestAnsSubmititem.Name = "simpBtnQuestAnsSubmititem";
+            this.simpBtnQuestAnsSubmititem.Size = new System.Drawing.Size(903, 51);
+            this.simpBtnQuestAnsSubmititem.TextSize = new System.Drawing.Size(0, 0);
+            this.simpBtnQuestAnsSubmititem.TextVisible = false;
             // 
             // FormQuestionAnswer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 438);
-            this.Controls.Add(this.simpBtnQuestAnsSubmit);
-            this.Controls.Add(this.radioGroupStuAnswerSection);
-            this.Controls.Add(this.labelControlStuSelectAnswer);
-            this.Controls.Add(this.lControlStuQuestion);
-            this.Controls.Add(this.memoEditStuQuestion);
+            this.ClientSize = new System.Drawing.Size(923, 472);
+            this.Controls.Add(this.FormQuestionAnswerlayoutControl1ConvertedLayout);
             this.Name = "FormQuestionAnswer";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "StuNote 21 - QuestionAnswer";
             this.TopMost = true;
             ((System.ComponentModel.ISupportInitialize)(this.memoEditStuQuestion.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radioGroupStuAnswerSection.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FormQuestionAnswerlayoutControl1ConvertedLayout)).EndInit();
+            this.FormQuestionAnswerlayoutControl1ConvertedLayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroupStuAnswerSectionitem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEditStuQuestionitem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.simpBtnQuestAnsSubmititem)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private DevExpress.XtraEditors.MemoEdit memoEditStuQuestion;
-        private DevExpress.XtraEditors.LabelControl lControlStuQuestion;
-        private DevExpress.XtraEditors.LabelControl labelControlStuSelectAnswer;
         private DevExpress.XtraEditors.RadioGroup radioGroupStuAnswerSection;
         private DevExpress.XtraEditors.SimpleButton simpBtnQuestAnsSubmit;
+        private DevExpress.XtraLayout.LayoutControl FormQuestionAnswerlayoutControl1ConvertedLayout;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControlItem radioGroupStuAnswerSectionitem;
+        private DevExpress.XtraLayout.LayoutControlItem memoEditStuQuestionitem;
+        private DevExpress.XtraLayout.LayoutControlItem simpBtnQuestAnsSubmititem;
     }
 }
--- StuNote.Student/FormSurveyAnswer.Designer.cs
+++ StuNote.Student/FormSurveyAnswer.Designer.cs
@@ -59,10 +59,9 @@ namespace StuNote.Student
             // 
             this.buttonSubmit.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Primary;
             this.buttonSubmit.Appearance.Options.UseBackColor = true;
-            this.buttonSubmit.Location = new System.Drawing.Point(12, 215);
-            this.buttonSubmit.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSubmit.Location = new System.Drawing.Point(18, 306);
             this.buttonSubmit.Name = "buttonSubmit";
-            this.buttonSubmit.Size = new System.Drawing.Size(509, 32);
+            this.buttonSubmit.Size = new System.Drawing.Size(959, 47);
             this.buttonSubmit.StyleController = this.FormSurveyAnswerlayoutControl1ConvertedLayout;
             this.buttonSubmit.TabIndex = 1;
             this.buttonSubmit.Text = "Submit";
@@ -75,34 +74,31 @@ namespace StuNote.Student
             this.FormSurveyAnswerlayoutControl1ConvertedLayout.Controls.Add(this.buttonSubmit);
             this.FormSurveyAnswerlayoutControl1ConvertedLayout.Dock = System.Windows.Forms.DockStyle.Fill;
             this.FormSurveyAnswerlayoutControl1ConvertedLayout.Location = new System.Drawing.Point(0, 0);
-            this.FormSurveyAnswerlayoutControl1ConvertedLayout.Margin = new System.Windows.Forms.Padding(2);
             this.FormSurveyAnswerlayoutControl1ConvertedLayout.Name = "FormSurveyAnswerlayoutControl1ConvertedLayout";
             this.FormSurveyAnswerlayoutControl1ConvertedLayout.Root = this.layoutControlGroup1;
-            this.FormSurveyAnswerlayoutControl1ConvertedLayout.Size = new System.Drawing.Size(533, 292);
+            this.FormSurveyAnswerlayoutControl1ConvertedLayout.Size = new System.Drawing.Size(995, 479);
             this.FormSurveyAnswerlayoutControl1ConvertedLayout.TabIndex = 6;
             // 
             // radioGroupStudentAnswer
             // 
             this.radioGroupStudentAnswer.AutoSizeInLayoutControl = true;
             this.radioGroupStudentAnswer.EditValue = "<Null>";
-            this.radioGroupStudentAnswer.Location = new System.Drawing.Point(58, 115);
-            this.radioGroupStudentAnswer.Margin = new System.Windows.Forms.Padding(2);
+            this.radioGroupStudentAnswer.Location = new System.Drawing.Point(85, 189);
             this.radioGroupStudentAnswer.Name = "radioGroupStudentAnswer";
             this.radioGroupStudentAnswer.Properties.Columns = 1;
             this.radioGroupStudentAnswer.Properties.Items.AddRange(new DevExpress.XtraEditors.Controls.RadioGroupItem[] {
             new DevExpress.XtraEditors.Controls.RadioGroupItem("Answer1", "Answer1"),
             new DevExpress.XtraEditors.Controls.RadioGroupItem("Answer2", "Answer2")});
-            this.radioGroupStudentAnswer.Size = new System.Drawing.Size(463, 86);
+            this.radioGroupStudentAnswer.Size = new System.Drawing.Size(892, 88);
             this.radioGroupStudentAnswer.StyleController = this.FormSurveyAnswerlayoutControl1ConvertedLayout;
             this.radioGroupStudentAnswer.TabIndex = 5;
             // 
             // StuSurveyQuestion
             // 
             this.StuSurveyQuestion.Enabled = false;
-            this.StuSurveyQuestion.Location = new System.Drawing.Point(58, 12);
-            this.StuSurveyQuestion.Margin = new System.Windows.Forms.Padding(2);
+            this.StuSurveyQuestion.Location = new System.Drawing.Point(85, 18);
             this.StuSurveyQuestion.Name = "StuSurveyQuestion";
-            this.StuSurveyQuestion.Size = new System.Drawing.Size(463, 89);
+            this.StuSurveyQuestion.Size = new System.Drawing.Size(892, 148);
             this.StuSurveyQuestion.StyleController = this.FormSurveyAnswerlayoutControl1ConvertedLayout;
             this.StuSurveyQuestion.TabIndex = 3;
             // 
@@ -118,71 +114,70 @@ namespace StuNote.Student
             this.emptySpaceItem2,
             this.emptySpaceItem3});
             this.layoutControlGroup1.Name = "layoutControlGroup1";
-            this.layoutControlGroup1.Size = new System.Drawing.Size(533, 292);
+            this.layoutControlGroup1.Size = new System.Drawing.Size(995, 479);
             this.layoutControlGroup1.TextVisible = false;
             // 
             // radioGroupStudentAnsweritem
             // 
             this.radioGroupStudentAnsweritem.Control = this.radioGroupStudentAnswer;
-            this.radioGroupStudentAnsweritem.Location = new System.Drawing.Point(0, 103);
+            this.radioGroupStudentAnsweritem.Location = new System.Drawing.Point(0, 171);
             this.radioGroupStudentAnsweritem.Name = "radioGroupStudentAnsweritem";
-            this.radioGroupStudentAnsweritem.Size = new System.Drawing.Size(513, 90);
+            this.radioGroupStudentAnsweritem.Size = new System.Drawing.Size(965, 94);
             this.radioGroupStudentAnsweritem.Text = "Answer";
             this.radioGroupStudentAnsweritem.TextLocation = DevExpress.Utils.Locations.Left;
-            this.radioGroupStudentAnsweritem.TextSize = new System.Drawing.Size(43, 13);
+            this.radioGroupStudentAnsweritem.TextSize = new System.Drawing.Size(63, 19);
             // 
             // StuSurveyQuestionitem
             // 
             this.StuSurveyQuestionitem.Control = this.StuSurveyQuestion;
             this.StuSurveyQuestionitem.Location = new System.Drawing.Point(0, 0);
             this.StuSurveyQuestionitem.Name = "StuSurveyQuestionitem";
-            this.StuSurveyQuestionitem.Size = new System.Drawing.Size(513, 93);
+            this.StuSurveyQuestionitem.Size = new System.Drawing.Size(965, 154);
             this.StuSurveyQuestionitem.Text = "Question";
             this.StuSurveyQuestionitem.TextLocation = DevExpress.Utils.Locations.Left;
-            this.StuSurveyQuestionitem.TextSize = new System.Drawing.Size(43, 13);
+            this.StuSurveyQuestionitem.TextSize = new System.Drawing.Size(63, 19);
             // 
             // buttonSubmititem
             // 
             this.buttonSubmititem.Control = this.buttonSubmit;
-            this.buttonSubmititem.Location = new System.Drawing.Point(0, 203);
+            this.buttonSubmititem.Location = new System.Drawing.Point(0, 288);
             this.buttonSubmititem.Name = "buttonSubmititem";
-            this.buttonSubmititem.Size = new System.Drawing.Size(513, 36);
+            this.buttonSubmititem.Size = new System.Drawing.Size(965, 53);
             this.buttonSubmititem.TextSize = new System.Drawing.Size(0, 0);
             this.buttonSubmititem.TextVisible = false;
             // 
             // emptySpaceItem1
             // 
             this.emptySpaceItem1.AllowHotTrack = false;
-            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 93);
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 154);
             this.emptySpaceItem1.Name = "emptySpaceItem1";
-            this.emptySpaceItem1.Size = new System.Drawing.Size(513, 10);
+            this.emptySpaceItem1.Size = new System.Drawing.Size(965, 17);
             this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
             // 
             // emptySpaceItem2
             // 
             this.emptySpaceItem2.AllowHotTrack = false;
-            this.emptySpaceItem2.Location = new System.Drawing.Point(0, 193);
+            this.emptySpaceItem2.Location = new System.Drawing.Point(0, 265);
             this.emptySpaceItem2.Name = "emptySpaceItem2";
-            this.emptySpaceItem2.Size = new System.Drawing.Size(513, 10);
+            this.emptySpaceItem2.Size = new System.Drawing.Size(965, 23);
             this.emptySpaceItem2.TextSize = new System.Drawing.Size(0, 0);
             // 
             // emptySpaceItem3
             // 
             this.emptySpaceItem3.AllowHotTrack = false;
-            this.emptySpaceItem3.Location = new System.Drawing.Point(0, 239);
+            this.emptySpaceItem3.Location = new System.Drawing.Point(0, 341);
             this.emptySpaceItem3.Name = "emptySpaceItem3";
-            this.emptySpaceItem3.Size = new System.Drawing.Size(513, 33);
+            this.emptySpaceItem3.Size = new System.Drawing.Size(965, 108);
             this.emptySpaceItem3.TextSize = new System.Drawing.Size(0, 0);
             // 
             // FormSurveyAnswer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.ClientSize = new System.Drawing.Size(995, 479);
             this.Controls.Add(this.FormSurveyAnswerlayoutControl1ConvertedLayout);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormSurveyAnswer";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
